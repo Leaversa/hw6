7e2231dce8351da4e7409f6e04903971a18d6ccb
Leaversa <mtkwong@cpp.edu>
2023-11-24 20:55:08 -0800

    max min comic number

--- src/xkcd.jsx
+++ src/xkcd.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { useEffect } from "react";
 
+const XkcdResults = ({ comic }) => {
+  if (comic === null) {
+    return <div style={{ textAlign: "center" }}>Loading...</div>;
+  }
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>{comic.title}</h2>
+      <img src={comic.img} alt={comic.alt} style={{ maxWidth: "100%" }} />
+      <p>{comic.alt}</p>
+      <p>Issue Number: {comic.num}</p>
+      <p>Date: {`${comic.day}-${comic.month}-${comic.year}`}</p>
+    </div>
+  );
+};
+
 function XkcdInfo() {
   const [comic, setComic] = React.useState(null);
+  const latest = React.useRef(null);
   const [form, setForm] = React.useState({
     comicNumber: "",
     randomComic: false,
@@ -11,7 +27,10 @@ function XkcdInfo() {
   const fetchComic = (issue) => {
     fetch(`https://xkcd.now.sh/?comic=${issue}`)
       .then((response) => response.json())
-      .then((data) => setComic(data));
+      .then((data) => {
+        setComic(data);
+        latest.current = data.num;
+      });
   };
   useEffect(() => {
     fetchComic("latest");
@@ -22,7 +41,7 @@ function XkcdInfo() {
     if (form.comicNumber === "" && form.randomComic === false) {
       fetchComic("latest");
     } else if (form.randomComic === true) {
-      const randomComicNum = Math.floor(Math.random() * 2859) + 1;
+      const randomComicNum = Math.floor(Math.random() * latest.current) + 1;
       setForm({ ...form, comicNumber: randomComicNum });
       fetchComic(randomComicNum);
     } else {
@@ -48,6 +67,8 @@ function XkcdInfo() {
           style={{ width: "225px", marginBottom: "10px" }}
           placeholder="Comic number (blank for latest)"
           disabled={form.randomComic}
+          max={latest.current}
+          min={1}
           value={form.comicNumber}
           onChange={(e) => setForm({ ...form, comicNumber: e.target.value })}
         />
@@ -73,15 +94,7 @@ function XkcdInfo() {
           Get Comic
         </button>
       </form>
-      {comic && (
-        <div style={{ textAlign: "center" }}>
-          <h2>{comic.title}</h2>
-          <img src={comic.img} alt={comic.alt} style={{ maxWidth: "100%" }} />
-          <p>{comic.alt}</p>
-          <p>Issue Number: {comic.num}</p>
-          <p>Date: {`${comic.day}-${comic.month}-${comic.year}`}</p>
-        </div>
-      )}
+      <XkcdResults comic={comic} />
     </div>
   );
 }
